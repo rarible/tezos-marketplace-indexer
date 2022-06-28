6e3503439eb5ae4eee2846629cfe8488047ae24a
Florian PAUTOT <florian.pautot@gmail.com>
2022-06-28 13:06:26 +0200

    add logger levels config

--- rarible_marketplace_indexer/hooks/on_restart.py
+++ rarible_marketplace_indexer/hooks/on_restart.py
@@ -1,3 +1,5 @@
+import logging
+
 from dipdup.context import HookContext
 
 from rarible_marketplace_indexer.producer.container import ProducerContainer
@@ -6,6 +8,9 @@ from rarible_marketplace_indexer.producer.container import ProducerContainer
 async def on_restart(
     ctx: HookContext,
 ) -> None:
+    logging.getLogger('dipdup').setLevel('INFO')
+    logging.getLogger('aiokafka').setLevel('INFO')
+    logging.getLogger('db_client').setLevel('INFO')
     await ctx.execute_sql('on_restart')
 
     ProducerContainer.create_instance(ctx.config.custom, ctx.logger)
