58665804f7257ccf602893e0ec204bb4050e7423
Florian PAUTOT <florian.pautot@gmail.com>
2022-08-11 09:31:15 +0200

    fix values for match activities

--- rarible_marketplace_indexer/event/abstract_action.py
+++ rarible_marketplace_indexer/event/abstract_action.py
@@ -319,7 +319,7 @@ class AbstractOrderMatchEvent(EventInterface):
         match_activity.taker = transaction.data.sender_address
 
         match_activity.make_value = dto.match_amount
-        match_activity.take_value = AssetValue(order.take_value * dto.match_amount)
+        match_activity.take_value = AssetValue(order.make_price * dto.match_amount)
 
         await match_activity.save()
 
@@ -356,6 +356,7 @@ class AbstractLegacyOrderMatchEvent(EventInterface):
             .first()
         )
 
+        make_price = dto.take.value / dto.make.value
         if order is None:
             order = await OrderModel.create(
                 network=datasource.network,
@@ -372,7 +373,7 @@ class AbstractLegacyOrderMatchEvent(EventInterface):
                 make_contract=dto.make.contract,
                 make_token_id=dto.make.token_id,
                 make_value=dto.make.value,
-                make_price=dto.take.value / dto.make.value,
+                make_price=make_price,
                 take_asset_class=dto.take.asset_class,
                 take_contract=dto.take.contract,
                 take_token_id=dto.take.token_id,
@@ -382,7 +383,7 @@ class AbstractLegacyOrderMatchEvent(EventInterface):
             )
         else:
             order.make_value = dto.make.value
-            order.take_value = dto.take.value
+            order.take_value = dto.take.value * dto.make.value
             order.origin_fees = get_json_parts(dto.origin_fees)
             order.payouts = get_json_parts(dto.payouts)
 
@@ -444,7 +445,7 @@ class AbstractLegacyOrderMatchEvent(EventInterface):
                 take_asset_class=dto.take.asset_class,
                 take_contract=dto.take.contract,
                 take_token_id=dto.take.token_id,
-                take_value=dto.take.value * dto.make.value,
+                take_value=make_price * dto.match_amount,
                 taker=transaction.data.sender_address,
                 operation_level=transaction.data.level,
                 operation_timestamp=transaction.data.timestamp,
