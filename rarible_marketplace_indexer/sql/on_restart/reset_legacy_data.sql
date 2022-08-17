519b1c78403f4ab3a93b0cb07a9c79acc229b717
Florian PAUTOT <florian.pautot@gmail.com>
2022-08-17 02:01:14 +0200

    amend reset script

--- rarible_marketplace_indexer/sql/on_restart/reset_legacy_data.sql
+++ rarible_marketplace_indexer/sql/on_restart/reset_legacy_data.sql
@@ -3,3 +3,4 @@ delete from marketplace_activity where platform = 'RARIBLE_V1';
 delete from legacy_orders;
 delete from dipdup_index where name = 'rarible_exchange_legacy_actions';
 delete from dipdup_index where name = 'rarible_exchange_legacy_data_actions';
+delete from indexing_status where index = 'LEGACY_ORDERS'
