a8c3952d93ee1ba01890e59129920ada2c3cfdc1
Florian PAUTOT <florian.pautot@gmail.com>
2022-08-17 11:31:59 +0200

    add cleaning job

--- rarible_marketplace_indexer/hooks/on_restart.py
+++ rarible_marketplace_indexer/hooks/on_restart.py
@@ -1,8 +1,12 @@
+import datetime
 import logging
 import os
 
+import requests
 from dipdup.context import HookContext
 
+from rarible_marketplace_indexer.models import IndexingStatus, IndexEnum, OrderModel, PlatformEnum, OrderStatusEnum, \
+    LegacyOrderModel
 from rarible_marketplace_indexer.producer.container import ProducerContainer
 
 
@@ -15,6 +19,45 @@ async def on_restart(
     await ctx.execute_sql('on_restart')
 
     ProducerContainer.create_instance(ctx.config.custom, ctx.logger)
+
+    legacy_cleaning = await IndexingStatus.get_or_none(index=IndexEnum.V1_CLEANING)
+    if legacy_cleaning is None:
+        logger = logging.getLogger('dipdup.legacy_cancel')
+        logger.info("Processing faulty legacy orders...")
+        orders = (
+            await OrderModel.filter(
+                platform=PlatformEnum.RARIBLE_V1,
+                status=OrderStatusEnum.ACTIVE
+            )
+        )
+        faulty_orders: [OrderModel] = []
+        i = 0
+        for order in orders:
+            legacy_order = (
+                await LegacyOrderModel.filter(
+                    id=order.id,
+                )
+                .order_by('-id')
+                .first()
+            )
+            if legacy_order is None:
+                faulty_orders.append(order)
+                logger.info(f"{i} - Cancelling order {order.id}")
+                order.status = OrderStatusEnum.CANCELLED
+                order.cancelled = True
+                order.ended_at = datetime.datetime.now()
+                order.last_updated_at = datetime.datetime.now()
+                await order.save()
+                i = i + 1
+                response = requests.post(f"{os.getenv('UNION_API')}/v0.1/refresh/item/TEZOS:{order.make_contract}:{order.make_token_id}/reconcile?full=true")
+                if not response.ok:
+                    logger.info(f"{order.make_contract}:{order.make_token_id} need reconcile: Error {response.status_code} - {response.reason}")
+                else:
+                    logger.info(f"{order.make_contract}:{order.make_token_id} synced properly after legacy cancel")
+        await IndexingStatus.create(index=IndexEnum.V1_CLEANING, last_level="DONE")
+        logger.info(f"Cleaned {len(faulty_orders)} orders")
+
     await ProducerContainer.get_instance().start()
     if os.getenv('APPLICATION_ENVIRONMENT') != 'dev':
         await ctx.fire_hook("import_legacy_orders")
+
