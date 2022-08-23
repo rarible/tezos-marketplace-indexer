31a3c04cda01c5a56ac01008ce371fc9c135b877
Florian PAUTOT <florian.pautot@gmail.com>
2022-08-23 15:34:24 +0200

    Migrate prometheus endpoint to expected port and path

--- rarible_marketplace_indexer/hooks/on_restart.py
+++ rarible_marketplace_indexer/hooks/on_restart.py
@@ -1,11 +1,15 @@
 import logging
 import os
+from threading import Thread
 
 from dipdup.context import HookContext
 
 from rarible_marketplace_indexer.producer.container import ProducerContainer
 from rarible_marketplace_indexer.prometheus.rarible_metrics import RaribleMetrics
-
+from prometheus_client.twisted import MetricsResource
+from twisted.web.server import Site
+from twisted.web.resource import Resource
+from twisted.internet import reactor
 
 async def on_restart(
     ctx: HookContext,
@@ -19,6 +23,12 @@ async def on_restart(
 
     if ctx.config.prometheus is not None:
         RaribleMetrics.enabled = True
-
+        root = Resource()
+        status = Resource()
+        root.putChild(b'_status', status)
+        status.putChild(b'prometheus', MetricsResource())
+        factory = Site(root)
+        reactor.listenTCP(8080, factory)
+        Thread(target=reactor.run, args=(False,)).start()
     if os.getenv('APPLICATION_ENVIRONMENT') == 'prod':
         await ctx.fire_hook("import_legacy_orders")
