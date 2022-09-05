7c45fcaa84c0bada515fff3fa76df330412df1a9
Florian PAUTOT <florian.pautot@gmail.com>
2022-09-05 23:40:30 +0200

    fix config and prometheus server

--- rarible_marketplace_indexer/hooks/on_restart.py
+++ rarible_marketplace_indexer/hooks/on_restart.py
@@ -1,18 +1,15 @@
 import asyncio
-import asyncio
 import logging
 import os
-from threading import Thread
+import socketserver
+import threading
 
 from dipdup.context import HookContext
-from prometheus_client.twisted import MetricsResource
-from twisted.internet import reactor
-from twisted.web.resource import Resource
-from twisted.web.server import Site
+from prometheus_client import MetricsHandler
 
-from rarible_marketplace_indexer.producer.container import ProducerContainer
 from rarible_marketplace_indexer.event.fxhash_v2_action import FxhashV2ListingOrderListEvent
 from rarible_marketplace_indexer.hooks.process_collection_events import process_collection_events
+from rarible_marketplace_indexer.producer.container import ProducerContainer
 from rarible_marketplace_indexer.prometheus.rarible_metrics import RaribleMetrics
 
 
@@ -22,19 +19,19 @@ async def on_restart(
     logging.getLogger('dipdup').setLevel('INFO')
     logging.getLogger('aiokafka').setLevel('INFO')
     logging.getLogger('db_client').setLevel('INFO')
+
     await ctx.execute_sql('on_restart')
     ProducerContainer.create_instance(ctx.config.custom, ctx.logger)
     await ProducerContainer.get_instance().start()
 
     if ctx.config.prometheus is not None:
         RaribleMetrics.enabled = True
-        root = Resource()
-        status = Resource()
-        root.putChild(b'_status', status)
-        status.putChild(b'prometheus', MetricsResource())
-        factory = Site(root)
-        reactor.listenTCP(8080, factory)
-        Thread(target=reactor.run, args=(False,)).start()
+        prometheus_port = 8080
+        metrics_handler = MetricsHandler
+        prometheus_httpd = socketserver.TCPServer(("", prometheus_port), metrics_handler)
+        prometheus_http_thread = threading.Thread(target=prometheus_httpd.serve_forever)
+        prometheus_http_thread.daemon = True
+        prometheus_http_thread.start()
 
     if ctx.config.indexes.get("fxhash_v2_actions") is not None:
         FxhashV2ListingOrderListEvent.fxhash_nft_addresses = {
