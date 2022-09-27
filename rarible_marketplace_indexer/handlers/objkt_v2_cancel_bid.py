ed17daa2001a1039f1fe925e51ce4e13dc229e6b
Florian PAUTOT <florian.pautot@gmail.com>
2022-09-28 00:40:56 +0200

    ECHO-314: add missing handler for objkt v2 bids

--- rarible_marketplace_indexer/handlers/objkt_v2_cancel_bid.py
+++ rarible_marketplace_indexer/handlers/objkt_v2_cancel_bid.py
@@ -1,4 +1,4 @@
-
+from rarible_marketplace_indexer.event.objkt_v2_action import ObjktV2CancelBidEvent
 from rarible_marketplace_indexer.types.objkt_marketplace_v2.storage import ObjktMarketplaceV2Storage
 from dipdup.context import HandlerContext
 from dipdup.models import Transaction
@@ -8,4 +8,4 @@ async def objkt_v2_cancel_bid(
     ctx: HandlerContext,
     retract_offer: Transaction[RetractOfferParameter, ObjktMarketplaceV2Storage],
 ) -> None:
-    ...
+    await ObjktV2CancelBidEvent.handle(retract_offer, ctx.datasource)
--- rarible_marketplace_indexer/handlers/objkt_v2_put_bid.py
+++ rarible_marketplace_indexer/handlers/objkt_v2_put_bid.py
@@ -1,4 +1,4 @@
-
+from rarible_marketplace_indexer.event.objkt_v2_action import ObjktV2PutBidEvent
 from rarible_marketplace_indexer.types.objkt_marketplace_v2.storage import ObjktMarketplaceV2Storage
 from dipdup.context import HandlerContext
 from dipdup.models import Transaction
@@ -8,4 +8,4 @@ async def objkt_v2_put_bid(
     ctx: HandlerContext,
     offer: Transaction[OfferParameter, ObjktMarketplaceV2Storage],
 ) -> None:
-    ...
+    await ObjktV2PutBidEvent.handle(offer, ctx.datasource)
