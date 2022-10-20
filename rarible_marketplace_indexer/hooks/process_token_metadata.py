b1e6bfb78285b218448cd63fbd7511c23019944a
Florian PAUTOT <florian.pautot@gmail.com>
2022-10-20 15:17:53 +0200

    fix logs for token meta

--- rarible_marketplace_indexer/hooks/process_token_metadata.py
+++ rarible_marketplace_indexer/hooks/process_token_metadata.py
@@ -45,6 +45,7 @@ async def process_metadata_for_token(ctx: HookContext, token_meta: TokenMetadata
             # token = await Token.get(id=token_meta.id)
             # event = RaribleApiTokenFactory.build_meta_update(token)
             # await producer_send(event)
+            log = f"Successfully saved metadata for {token_meta.contract}:{token_meta.token_id} (retries {token_meta.metadata_retries})"
         except Exception as ex:
             log = f"Could not save token metadata for {token_meta.contract}:{token_meta.token_id}: {ex}"
             token_meta.metadata_retries = token_meta.metadata_retries + 1
