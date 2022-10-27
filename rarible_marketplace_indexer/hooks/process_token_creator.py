56279c0e7bdfddaf56387fdcaf54e3568102890c
Florian PAUTOT <florian.pautot@gmail.com>
2022-10-27 09:24:19 +0200

    improve creator and royalties jobs

--- rarible_marketplace_indexer/hooks/process_token_creator.py
+++ rarible_marketplace_indexer/hooks/process_token_creator.py
@@ -16,23 +16,23 @@ logger.setLevel("INFO")
 
 async def process_creator_for_token(ctx: HookContext, token: Token):
     royalties = await Royalties.get(id=token.id)
-    #log = ""
+    log = ""
     if royalties is not None and len(royalties.parts) > 0:
-    #     mint: TokenTransfer = await TokenTransfer.get(contract=token.contract, token_id=token.token_id, type=ActivityTypeEnum.TOKEN_MINT)
-    #     if mint is not None:
-    #         token.creator = mint.to_address
-    #         await token.save()
-    #         log = f"Creator not found for {token.contract}:{token.token_id}, using first owner"
-    #     else:
-    #         log = f"Creator not found for {token.contract}:{token.token_id}"
-    # else:
-    #     try:
-        token.creator = royalties.parts[0].get('part_account')
-        await token.save()
-        logger.info(f"Successfully saved creator for {token.contract}:{token.token_id}")
-        # except Exception as ex:
-        #     log = f"Could not save creator for {token.contract}:{token.token_id}: {ex}"
-    #logger.info(log)
+        mint: TokenTransfer = await TokenTransfer.get(contract=token.contract, token_id=token.token_id, type=ActivityTypeEnum.TOKEN_MINT)
+        if mint is not None:
+            token.creator = mint.to_address
+            await token.save()
+            log = f"Creator not found for {token.contract}:{token.token_id}, using first owner"
+        else:
+            log = f"Creator not found for {token.contract}:{token.token_id}"
+    else:
+        try:
+            token.creator = royalties.parts[0].get('part_account')
+            await token.save()
+            log = f"Successfully saved creator for {token.contract}:{token.token_id}"
+        except Exception as ex:
+            log = f"Could not save creator for {token.contract}:{token.token_id}: {ex}"
+    logger.info(log)
 
 async def process_token_creator(
     ctx: HookContext,
@@ -47,7 +47,7 @@ async def process_token_creator(
     while not done:
         unsynced_creators: List[Token] = await Token.filter(
             creator=None,
-        ).limit(100).offset(offset).order_by("-db_updated_at")
+        ).limit(100).offset(offset)
 
         if len(unsynced_creators) == 0:
             done = True
--- rarible_marketplace_indexer/hooks/process_token_royalties.py
+++ rarible_marketplace_indexer/hooks/process_token_royalties.py
@@ -25,12 +25,15 @@ async def process_royalties_for_token(ctx: HookContext, token_royalties: Royalti
         token_royalties.royalties_retries = token_royalties.royalties_retries + 1
         token_royalties.royalties_synced = False
         log = f"Royalties not found for {token_royalties.contract}:{token_royalties.token_id} (retries {token_royalties.royalties_retries})"
-
     else:
         try:
             token_royalties.parts = get_json_parts(royalties)
             token_royalties.royalties_synced = True
             token_royalties.royalties_retries = token_royalties.royalties_retries
+            token = await Token.get(id=Token.get_id(contract=token_royalties.contract,
+                                                    token_id=token_royalties.token_id))
+            token.creator = royalties[0].part_account
+            await token.save()
             log = f"Successfully saved royalties for {token_royalties.contract}:{token_royalties.token_id} (retries {token_royalties.royalties_retries})"
         except Exception as ex:
             log = f"Could not save royalties for {token_royalties.contract}:{token_royalties.token_id}: {ex}"
