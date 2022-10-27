f990de7f2f8033738a8c708e0b45950a354e25ba
Roman Novikov <mystdeim@gmail.com>
2022-10-27 18:48:25 +0100

    draft

--- rarible_marketplace_indexer/handlers/on_transfer.py
+++ rarible_marketplace_indexer/handlers/on_transfer.py
@@ -6,8 +6,9 @@ from dipdup.context import HandlerContext
 from dipdup.enums import TokenStandard
 from dipdup.models import TokenTransferData
 
+from rarible_marketplace_indexer.handlers.ownership.ownership_reduce import ownership_transfer
 from rarible_marketplace_indexer.models import ActivityTypeEnum
-from rarible_marketplace_indexer.models import Ownership
+# from rarible_marketplace_indexer.models import Ownership
 from rarible_marketplace_indexer.models import Royalties
 from rarible_marketplace_indexer.models import Token
 from rarible_marketplace_indexer.models import TokenMetadata
@@ -22,9 +23,9 @@ async def on_transfer(ctx: HandlerContext, token_transfer: TokenTransferData) ->
             null_addresses = [None, "tz1burnburnburnburnburnburnburjAYjjX", "tz1Ke2h7sDdakHJQh8WX4Z372du1KChsksyU"]
             transfer = await TokenTransfer.get_or_none(id=token_transfer.id)
             token_id = Token.get_id(token_transfer.contract_address, token_transfer.token_id)
-            ownership_id = Ownership.get_id(
-                token_transfer.contract_address, token_transfer.token_id, token_transfer.to_address
-            )
+            # ownership_id = Ownership.get_id(
+            #     token_transfer.contract_address, token_transfer.token_id, token_transfer.to_address
+            # )
             if transfer is None:
                 is_mint = token_transfer.from_address is None
                 minted = None
@@ -34,16 +35,16 @@ async def on_transfer(ctx: HandlerContext, token_transfer: TokenTransferData) ->
                 is_burn = token_transfer.to_address is None or token_transfer.to_address in null_addresses
                 if is_burn:
                     burned = await Token.get_or_none(id=token_id)
-                is_transfer_to = (
-                    token_transfer.to_address is not None
-                    and token_transfer.to_address not in null_addresses
-                    and token_transfer.amount > 0
-                )
-                if is_transfer_to:
-                    ownership_to = await Ownership.get_or_none(id=ownership_id)
-                is_transfer_from = token_transfer.from_address is not None and token_transfer.amount > 0
-                if is_transfer_from:
-                    ownership_from = await Ownership.get_or_none(id=ownership_id)
+                # is_transfer_to = (
+                #     token_transfer.to_address is not None
+                #     and token_transfer.to_address not in null_addresses
+                #     and token_transfer.amount > 0
+                # )
+                # if is_transfer_to:
+                #     ownership_to = await Ownership.get_or_none(id=ownership_id)
+                # is_transfer_from = token_transfer.from_address is not None and token_transfer.amount > 0
+                # if is_transfer_from:
+                #     ownership_from = await Ownership.get_or_none(id=ownership_id)
 
                 # persist
                 if is_mint:
@@ -100,29 +101,29 @@ async def on_transfer(ctx: HandlerContext, token_transfer: TokenTransferData) ->
                         burned.updated = token_transfer.timestamp
                         await burned.save()
 
-                if is_transfer_to:
-                    if ownership_to is None:
-                        ownership_to = Ownership(
-                            id=ownership_id,
-                            contract=token_transfer.contract_address,
-                            token_id=token_transfer.token_id,
-                            owner=token_transfer.to_address,
-                            balance=token_transfer.amount,
-                            updated=token_transfer.timestamp,
-                            created=token_transfer.timestamp,
-                        )
-                    else:
-                        ownership_to.balance += token_transfer.amount
-                        ownership_to.updated = token_transfer.timestamp
-                    await ownership_to.save()
+                # if is_transfer_to:
+                #     if ownership_to is None:
+                #         ownership_to = Ownership(
+                #             id=ownership_id,
+                #             contract=token_transfer.contract_address,
+                #             token_id=token_transfer.token_id,
+                #             owner=token_transfer.to_address,
+                #             balance=token_transfer.amount,
+                #             updated=token_transfer.timestamp,
+                #             created=token_transfer.timestamp,
+                #         )
+                #     else:
+                #         ownership_to.balance += token_transfer.amount
+                #         ownership_to.updated = token_transfer.timestamp
+                #     await ownership_to.save()
 
-                if is_transfer_from and ownership_from is not None:
-                    ownership_from.balance -= token_transfer.amount
-                    ownership_from.updated = token_transfer.timestamp
-                    if ownership_from.balance > 0:
-                        await ownership_from.save()
-                    else:
-                        await ownership_from.delete()
+                # if is_transfer_from and ownership_from is not None:
+                #     ownership_from.balance -= token_transfer.amount
+                #     ownership_from.updated = token_transfer.timestamp
+                #     if ownership_from.balance > 0:
+                #         await ownership_from.save()
+                #     else:
+                #         await ownership_from.delete()
 
                 activity_type = ActivityTypeEnum.TOKEN_TRANSFER
                 if is_mint:
@@ -144,3 +145,5 @@ async def on_transfer(ctx: HandlerContext, token_transfer: TokenTransferData) ->
                         to_address=token_transfer.to_address,
                         amount=token_transfer.amount,
                     ).save()
+
+                await ownership_transfer(token_transfer)
