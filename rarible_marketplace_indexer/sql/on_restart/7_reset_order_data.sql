db2c88ccde3f4a0e8e44a7b5c341c5ee5250bfe6
Florian PAUTOT <florian.pautot@gmail.com>
2022-10-28 23:15:17 +0200

    add reset script for v2 orders

--- rarible_marketplace_indexer/sql/on_restart/7_reset_order_data.sql
+++ rarible_marketplace_indexer/sql/on_restart/7_reset_order_data.sql
@@ -6,13 +6,17 @@
 --delete from indexing_status where index = 'LEGACY_ORDERS';
 --delete from indexing_status where index = 'V1_CLEANING';
 
+
+delete from marketplace_order where platform = 'RARIBLE_V2';
+delete from marketplace_activity where platform = 'RARIBLE_V2';
+
 -- delete from dipdup_index where name = 'fxhash_v1_actions';
 -- delete from dipdup_index where name = 'fxhash_v2_actions';
 -- delete from dipdup_index where name = 'hen_actions';
 -- delete from dipdup_index where name = 'objkt_v1_actions';
 -- delete from dipdup_index where name = 'objkt_v2_actions';
 -- delete from dipdup_index where name = 'rarible_bids_actions';
--- delete from dipdup_index where name = 'rarible_exchange_actions';
+delete from dipdup_index where name = 'rarible_exchange_actions';
 -- delete from dipdup_index where name = 'rarible_exchange_legacy_actions';
 -- delete from dipdup_index where name = 'rarible_exchange_legacy_data_actions';
 -- delete from dipdup_index where name = 'teia_v1_actions';
