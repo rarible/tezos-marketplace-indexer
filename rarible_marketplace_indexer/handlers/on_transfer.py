09b3ca62f8384b18526cb7693460d24bf26a6a48
Roman Novikov <mystdeim@gmail.com>
2022-10-30 13:19:42 +0300

    recalculation ownerships (#44)

--- rarible_marketplace_indexer/handlers/on_transfer.py
+++ rarible_marketplace_indexer/handlers/on_transfer.py
@@ -6,8 +6,8 @@ from dipdup.context import HandlerContext
 from dipdup.enums import TokenStandard
 from dipdup.models import TokenTransferData
 
+from rarible_marketplace_indexer.handlers.ownership.ownership_reduce import ownership_transfer
 from rarible_marketplace_indexer.models import ActivityTypeEnum
-from rarible_marketplace_indexer.models import Ownership
 from rarible_marketplace_indexer.models import Royalties
 from rarible_marketplace_indexer.models import Token
 from rarible_marketplace_indexer.models import TokenMetadata
@@ -17,14 +17,11 @@ from rarible_marketplace_indexer.utils.rarible_utils import date_pattern, assert
 
 async def on_transfer(ctx: HandlerContext, token_transfer: TokenTransferData) -> None:
     logger = logging.getLogger('dipdup.on_transfer')
-    if assert_token_id_length(str(token_transfer.token_id)) is True:
-        if token_transfer.standard == TokenStandard.FA2 and token_transfer.tzkt_transaction_id is not None:
+    if assert_token_id_length(str(token_transfer.token_id)):
+        if token_transfer.standard == TokenStandard.FA2:
             null_addresses = [None, "tz1burnburnburnburnburnburnburjAYjjX", "tz1Ke2h7sDdakHJQh8WX4Z372du1KChsksyU"]
             transfer = await TokenTransfer.get_or_none(id=token_transfer.id)
             token_id = Token.get_id(token_transfer.contract_address, token_transfer.token_id)
-            ownership_id = Ownership.get_id(
-                token_transfer.contract_address, token_transfer.token_id, token_transfer.to_address
-            )
             if transfer is None:
                 is_mint = token_transfer.from_address is None
                 minted = None
@@ -34,16 +31,6 @@ async def on_transfer(ctx: HandlerContext, token_transfer: TokenTransferData) ->
                 is_burn = token_transfer.to_address is None or token_transfer.to_address in null_addresses
                 if is_burn:
                     burned = await Token.get_or_none(id=token_id)
-                is_transfer_to = (
-                    token_transfer.to_address is not None
-                    and token_transfer.to_address not in null_addresses
-                    and token_transfer.amount > 0
-                )
-                if is_transfer_to:
-                    ownership_to = await Ownership.get_or_none(id=ownership_id)
-                is_transfer_from = token_transfer.from_address is not None and token_transfer.amount > 0
-                if is_transfer_from:
-                    ownership_from = await Ownership.get_or_none(id=ownership_id)
 
                 # persist
                 if is_mint:
@@ -100,30 +87,6 @@ async def on_transfer(ctx: HandlerContext, token_transfer: TokenTransferData) ->
                         burned.updated = token_transfer.timestamp
                         await burned.save()
 
-                if is_transfer_to:
-                    if ownership_to is None:
-                        ownership_to = Ownership(
-                            id=ownership_id,
-                            contract=token_transfer.contract_address,
-                            token_id=token_transfer.token_id,
-                            owner=token_transfer.to_address,
-                            balance=token_transfer.amount,
-                            updated=token_transfer.timestamp,
-                            created=token_transfer.timestamp,
-                        )
-                    else:
-                        ownership_to.balance += token_transfer.amount
-                        ownership_to.updated = token_transfer.timestamp
-                    await ownership_to.save()
-
-                if is_transfer_from and ownership_from is not None:
-                    ownership_from.balance -= token_transfer.amount
-                    ownership_from.updated = token_transfer.timestamp
-                    if ownership_from.balance > 0:
-                        await ownership_from.save()
-                    else:
-                        await ownership_from.delete()
-
                 activity_type = ActivityTypeEnum.TOKEN_TRANSFER
                 if is_mint:
                     activity_type = ActivityTypeEnum.TOKEN_MINT
@@ -138,9 +101,13 @@ async def on_transfer(ctx: HandlerContext, token_transfer: TokenTransferData) ->
                         date=token_transfer.timestamp,
                         tzkt_token_id=token_transfer.tzkt_token_id,
                         tzkt_transaction_id=token_transfer.tzkt_transaction_id,
+                        tzkt_origination_id=token_transfer.tzkt_origination_id,
                         contract=token_transfer.contract_address,
                         token_id=token_transfer.token_id,
                         from_address=token_transfer.from_address,
                         to_address=token_transfer.to_address,
                         amount=token_transfer.amount,
                     ).save()
+
+            # always recalculate transfers
+            await ownership_transfer(token_transfer)
---
+++ rarible_marketplace_indexer/handlers/ownership/ownership_reduce.py
@@ -0,0 +1,62 @@
+import logging
+from dipdup.models import TokenTransferData
+from tortoise import Tortoise
+
+from rarible_marketplace_indexer.models import Ownership
+
+logger = logging.getLogger('dipdup.ownership_reduce')
+
+
+async def ownership_transfer(transfer: TokenTransferData) -> None:
+    tasks = []
+    if transfer.to_address is not None:
+        tasks.append(process(transfer.contract_address, transfer.token_id, transfer.to_address, transfer.timestamp))
+    if transfer.from_address is not None:
+        tasks.append(process(transfer.contract_address, transfer.token_id, transfer.from_address, transfer.timestamp))
+    for task in tasks:
+        await task
+
+
+async def process(contract, token_id, owner, timestamp) -> None:
+    amount = await ownership_balance(contract, token_id, owner)
+    ownership_id = Ownership.get_id(contract, token_id, owner)
+    ownership = await Ownership.get_or_none(id=ownership_id)
+    if amount > 0:
+        if ownership is not None:
+            ownership.balance = amount
+            ownership.updated = timestamp
+        else:
+            ownership = Ownership(
+                id=ownership_id,
+                contract=contract,
+                token_id=token_id,
+                owner=owner,
+                balance=amount,
+                updated=timestamp,
+                created=timestamp,
+            )
+        await ownership.save()
+    if amount == 0 and ownership is not None:
+        await ownership.delete()
+
+
+async def ownership_balance(contract, token_id, owner) -> None:
+    conn = Tortoise.get_connection("default")
+    result = await conn.execute_query(f"""
+    select
+        sum(case when to_address = $1 then amount
+                 when from_address = $1 then -amount
+                 else 0
+            end)
+    from token_transfer where contract = $2
+                          and token_id = $3
+                          -- pg the comparison of NULL with a value will always result in NULL
+                          and ((to_address <> from_address) isnull or (to_address <> from_address))
+    """, [str(owner), str(contract), str(token_id)])
+    amount = result[1][0]['sum']
+    if amount is not None:
+        if amount < 0:
+            logger.error(f"Amount({amount}) mustn't be negative for ownership: {contract}:{token_id}:{owner}")
+        return amount
+    else:
+        return 0
