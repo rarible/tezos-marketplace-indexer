235ee1012f0f6fc93601cc1d01222c332a05e275
Roman Novikov <mystdeim@gmail.com>
2022-11-15 16:11:07 +0000

    fix running task

--- rarible_marketplace_indexer/hooks/process_tasks.py
+++ rarible_marketplace_indexer/hooks/process_tasks.py
@@ -11,5 +11,9 @@ async def process_tasks(ctx: HookContext):
     logger.debug('Looking for new or running tasks...')
     tasks = await Tasks.filter(status__in=['NEW', 'RUNNING'])
     for task in tasks:
-        logger.info(f"Starting task={task.name} with param={task.param}")
-        await ctx.fire_hook(task.name, wait=False, id=task.id)
+        if task.status == 'NEW' and task.sample is None:
+            logger.info(f"Starting task={task.name} with param={task.param}")
+            await ctx.fire_hook(task.name, wait=False, id=task.id)
+        elif task.status == 'RUNNING' and task.sample is not None:
+            logger.info(f"Continuing task={task.name} with param={task.param} and sample={task.sample}")
+            await ctx.fire_hook(task.name, wait=False, id=task.id)
