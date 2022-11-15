3fcca6281fb8a9debb276bb229838ea2e6294b65
Roman Novikov <mystdeim@gmail.com>
2022-11-15 16:34:51 +0000

    disabled unused sqls

--- rarible_marketplace_indexer/sql/on_restart/8_add_db_updated_at_field.sql
+++ rarible_marketplace_indexer/sql/on_restart/8_add_db_updated_at_field.sql
@@ -1,21 +1,21 @@
-DO $$
-    BEGIN
-        BEGIN
-            ALTER TABLE token_transfer ADD COLUMN db_updated_at timestamptz;
-        EXCEPTION
-            WHEN duplicate_column THEN RAISE NOTICE 'column db_updated_at already exists in token_transfer.';
-        END;
-    END;
-$$;
-create index concurrently if not exists token_transfer_db_updated_at on token_transfer (db_updated_at);
-
-DO $$
-    BEGIN
-        BEGIN
-            ALTER TABLE marketplace_activity ADD COLUMN db_updated_at timestamptz;
-        EXCEPTION
-            WHEN duplicate_column THEN RAISE NOTICE 'column db_updated_at already exists in marketplace_activity.';
-        END;
-    END;
-$$;
-create index concurrently if not exists marketplace_activity_db_updated_at on marketplace_activity (db_updated_at);
+-- DO $$
+--     BEGIN
+--         BEGIN
+--             ALTER TABLE token_transfer ADD COLUMN db_updated_at timestamptz;
+--         EXCEPTION
+--             WHEN duplicate_column THEN RAISE NOTICE 'column db_updated_at already exists in token_transfer.';
+--         END;
+--     END;
+-- $$;
+-- create index concurrently if not exists token_transfer_db_updated_at on token_transfer (db_updated_at);
+--
+-- DO $$
+--     BEGIN
+--         BEGIN
+--             ALTER TABLE marketplace_activity ADD COLUMN db_updated_at timestamptz;
+--         EXCEPTION
+--             WHEN duplicate_column THEN RAISE NOTICE 'column db_updated_at already exists in marketplace_activity.';
+--         END;
+--     END;
+-- $$;
+-- create index concurrently if not exists marketplace_activity_db_updated_at on marketplace_activity (db_updated_at);
--- rarible_marketplace_indexer/sql/on_restart/order_data.sql
+++ rarible_marketplace_indexer/sql/on_restart/order_data.sql
@@ -1,9 +1,9 @@
-DO $$
-    BEGIN
-        BEGIN
-            ALTER TABLE marketplace_order ADD COLUMN is_bid boolean NOT NULL DEFAULT false;
-        EXCEPTION
-            WHEN duplicate_column THEN RAISE NOTICE 'column <column_name> already exists in <table_name>.';
-        END;
-    END;
-$$
+-- DO $$
+--     BEGIN
+--         BEGIN
+--             ALTER TABLE marketplace_order ADD COLUMN is_bid boolean NOT NULL DEFAULT false;
+--         EXCEPTION
+--             WHEN duplicate_column THEN RAISE NOTICE 'column <column_name> already exists in <table_name>.';
+--         END;
+--     END;
+-- $$
