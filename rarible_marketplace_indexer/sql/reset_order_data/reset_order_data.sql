6de18a808eb08efbe50ad519bd4a234a84be61bb
Florian PAUTOT <florian.pautot@gmail.com>
2022-11-28 16:06:53 +0100

    reset rarible orders

--- rarible_marketplace_indexer/sql/reset_order_data/reset_order_data.sql
+++ rarible_marketplace_indexer/sql/reset_order_data/reset_order_data.sql
@@ -7,40 +7,40 @@
 --delete from indexing_status where index = 'V1_CLEANING';
 
 
--- delete from marketplace_order where platform = 'RARIBLE_V2';
--- delete from marketplace_activity where platform = 'RARIBLE_V2';
+delete from marketplace_order where platform = 'RARIBLE_V2';
+delete from marketplace_activity where platform = 'RARIBLE_V2';
 
-delete from marketplace_order where platform = 'TEIA_V1';
-delete from marketplace_activity where platform = 'TEIA_V1';
-
-delete from marketplace_order where platform = 'FXHASH_V1';
-delete from marketplace_activity where platform = 'FXHASH_V1';
-
-delete from marketplace_order where platform = 'FXHASH_V2';
-delete from marketplace_activity where platform = 'FXHASH_V2';
-
-delete from marketplace_order where platform = 'OBJKT_V2';
-delete from marketplace_activity where platform = 'OBJKT_V2';
-
-delete from marketplace_order where platform = 'OBJKT_V1';
-delete from marketplace_activity where platform = 'OBJKT_V1';
-
-delete from marketplace_order where platform = 'VERSUM_V1';
-delete from marketplace_activity where platform = 'VERSUM_V1';
-
-delete from marketplace_order where platform = 'HEN';
-delete from marketplace_activity where platform = 'HEN';
-
-delete from dipdup_index where name = 'fxhash_v1_actions';
-delete from dipdup_index where name = 'fxhash_v2_actions';
-delete from dipdup_index where name = 'hen_actions';
-delete from dipdup_index where name = 'objkt_v1_actions';
-delete from dipdup_index where name = 'objkt_v2_actions';
--- delete from dipdup_index where name = 'rarible_exchange_actions';
+-- delete from marketplace_order where platform = 'TEIA_V1';
+-- delete from marketplace_activity where platform = 'TEIA_V1';
+--
+-- delete from marketplace_order where platform = 'FXHASH_V1';
+-- delete from marketplace_activity where platform = 'FXHASH_V1';
+--
+-- delete from marketplace_order where platform = 'FXHASH_V2';
+-- delete from marketplace_activity where platform = 'FXHASH_V2';
+--
+-- delete from marketplace_order where platform = 'OBJKT_V2';
+-- delete from marketplace_activity where platform = 'OBJKT_V2';
+--
+-- delete from marketplace_order where platform = 'OBJKT_V1';
+-- delete from marketplace_activity where platform = 'OBJKT_V1';
+--
+-- delete from marketplace_order where platform = 'VERSUM_V1';
+-- delete from marketplace_activity where platform = 'VERSUM_V1';
+--
+-- delete from marketplace_order where platform = 'HEN';
+-- delete from marketplace_activity where platform = 'HEN';
+--
+-- delete from dipdup_index where name = 'fxhash_v1_actions';
+-- delete from dipdup_index where name = 'fxhash_v2_actions';
+-- delete from dipdup_index where name = 'hen_actions';
+-- delete from dipdup_index where name = 'objkt_v1_actions';
+-- delete from dipdup_index where name = 'objkt_v2_actions';
+delete from dipdup_index where name = 'rarible_exchange_actions';
 -- delete from dipdup_index where name = 'rarible_exchange_legacy_actions';
 -- delete from dipdup_index where name = 'rarible_exchange_legacy_data_actions';
-delete from dipdup_index where name = 'teia_v1_actions';
-delete from dipdup_index where name = 'versum';
+-- delete from dipdup_index where name = 'teia_v1_actions';
+-- delete from dipdup_index where name = 'versum';
 -- delete from indexing_status where index = 'LEGACY_ORDERS';
 -- delete from indexing_status where index = 'V1_CLEANING';
 -- delete from indexing_status where index = 'V1_FILL_FIX';
