17773c7259fea5f3c5c52baba8aa5d2175d94875
Florian PAUTOT <florian.pautot@gmail.com>
2022-12-12 17:04:11 +0100

    add example for reindex

--- rarible_marketplace_indexer/hooks/reprocess_transactions.py
+++ rarible_marketplace_indexer/hooks/reprocess_transactions.py
@@ -7,6 +7,7 @@ logger = logging.getLogger("dipdup.reprocess_tx")
 
 async def reprocess_transactions(ctx: HookContext, index_name: str, first_level: int, last_level: int) \
         -> None:
+    logger.info(f"Reindexing index {index_name} from level {first_level} to {last_level}")
     index_config = ctx.config.indexes.get(index_name)
     await ctx.add_index(
         name=index_name + '_reindex',
@@ -14,5 +15,7 @@ async def reprocess_transactions(ctx: HookContext, index_name: str, first_level:
         values={
             'contract': index_config.template_values.get("contract"),
         },
+        first_level=first_level,
+        last_level=last_level
     )
 
