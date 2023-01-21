92c0697d0dff48f2cf018a15546e6a3f2cfe4cee
Roman Novikov <mystdeim@gmail.com>
2023-01-21 00:54:29 +0000

    revert

--- rarible_marketplace_indexer/sql/on_restart/9_clean_royalties.sql
+++ rarible_marketplace_indexer/sql/on_restart/9_clean_royalties.sql
@@ -1,1 +1,1 @@
-update royalties set royalties_retries = 0 where royalties_synced = false and royalties_retries = 5;
+-- update royalties set royalties_retries = 0 where royalties_synced = false and royalties_retries = 5;
