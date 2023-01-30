c0626dbb4b564b144faff9b1db370b67f05ecd6b
Roman Novikov <mystdeim@gmail.com>
2023-01-30 17:22:19 +0000

    add deleting

--- rarible_marketplace_indexer/hooks/remove_reverted_transfers.py
+++ rarible_marketplace_indexer/hooks/remove_reverted_transfers.py
@@ -23,8 +23,14 @@ async def remove_reverted_transfers(ctx: HookContext, id: int) -> None:
         ).limit(1000)
         logger.info(f"Deleting transfers without hash (reverted) before {d}")
         for transfer in transfers:
-            await update_ownership(transfer, transfer.to_address)
-            await update_ownership(transfer, transfer.from_address)
+            contract = transfer.contract
+            token_id = transfer.token_id
+            to_address = transfer.to_address
+            from_address = transfer.from_address
+            date = transfer.date
+            await transfer.delete()
+            await update_ownership(contract, token_id, to_address, date)
+            await update_ownership(contract, token_id, from_address, date)
         logger.info(f"Task={task.name} finished")
         task.status = TaskStatus.FINISHED
     except Exception as err:
@@ -36,8 +42,8 @@ async def remove_reverted_transfers(ctx: HookContext, id: int) -> None:
     task.version += 1
     await task.save()
 
-async def update_ownership(transfer, owner):
+async def update_ownership(contract, token_id, owner, date):
     null_addresses = [None, "tz1burnburnburnburnburnburnburjAYjjX", "tz1Ke2h7sDdakHJQh8WX4Z372du1KChsksyU"]
     if owner not in null_addresses:
-        logger.info(f"Updating ownership: {transfer.contract}:{transfer.token_id}:{owner}")
-        await process(transfer.contract, transfer.token_id, owner, transfer.date)
+        logger.info(f"Updating ownership: {contract}:{token_id}:{owner}")
+        await process(contract, token_id, owner, date)
