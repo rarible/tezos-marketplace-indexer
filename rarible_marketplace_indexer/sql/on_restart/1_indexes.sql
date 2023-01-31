36884968f5da2551479a72bf46864da08a0c4c5b
Roman Novikov <mystdeim@gmail.com>
2023-01-31 13:37:57 +0000

    add owner index

--- rarible_marketplace_indexer/sql/on_restart/1_indexes.sql
+++ rarible_marketplace_indexer/sql/on_restart/1_indexes.sql
@@ -8,3 +8,4 @@ create index concurrently if not exists marketplace_activity_operation_timestamp
 create index concurrently if not exists marketplace_activity_type_operation_timestamp on marketplace_activity (type, operation_timestamp);
 create index concurrently if not exists token_transfer_date on token_transfer (date);
 create index concurrently if not exists marketplace_activity_request_by_item on marketplace_activity (make_contract, make_token_id);
+create index concurrently if not exists ownership_owner on ownership (owner);
