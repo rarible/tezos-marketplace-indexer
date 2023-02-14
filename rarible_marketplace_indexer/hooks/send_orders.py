2e13348863089d303c5f9c7a6ea7a5800fddfaad
Roman Novikov <mystdeim@gmail.com>
2023-02-14 16:17:22 +0000

    send orders

---
+++ rarible_marketplace_indexer/hooks/send_orders.py
@@ -0,0 +1,38 @@
+import logging
+import traceback
+
+from dipdup.context import HookContext
+
+from rarible_marketplace_indexer.enums import TaskStatus
+from rarible_marketplace_indexer.models import Tasks, Order
+from rarible_marketplace_indexer.producer.container import producer_send
+from rarible_marketplace_indexer.types.rarible_api_objects.order.factory import RaribleApiOrderFactory
+
+logger = logging.getLogger("dipdup.send_orders")
+
+
+async def send_orders(ctx: HookContext, id: int) -> None:
+    task = await Tasks.get_or_none(id=id)
+    try:
+        task.status = TaskStatus.RUNNING
+        await task.save()
+        platform = task.param
+        request = Order.filter(platform=platform, internal_order_id__lt=id) if task.sample is not None else Order.filter(platform=platform)
+        orders = await request.order_by('-internal_order_id').limit(1000)
+        if len(orders) > 0:
+            for order in orders:
+                event = RaribleApiOrderFactory.build(order)
+                await producer_send(event)
+            task.sample = orders[-1].internal_order_id
+            logger.info(f"Task={task.name} sent {len(orders)} orders, set sample={task.sample}")
+        else:
+            logger.info(f"Task={task.name} finished")
+            task.status = TaskStatus.FINISHED
+    except Exception as err:
+        str = traceback.format_exc()
+        task.error = str
+        task.status = TaskStatus.FAILED
+        logger.error(f"Task={task.name} failed with {err}")
+        logger.error(f"Task={task.name} trace: {str}")
+    task.version += 1
+    await task.save()
