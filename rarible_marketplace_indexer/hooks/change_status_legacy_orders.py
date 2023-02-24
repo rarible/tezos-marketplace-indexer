8db26f0be924ed44cd16bdfa503ac0cbda0e81b3
Roman Novikov <mystdeim@gmail.com>
2023-02-24 15:32:56 +0000

    fix legacy job

--- rarible_marketplace_indexer/hooks/change_status_legacy_orders.py
+++ rarible_marketplace_indexer/hooks/change_status_legacy_orders.py
@@ -13,11 +13,23 @@ async def change_status_legacy_orders(ctx: HookContext, batch):
     logger.info(f'Starting change_status_legacy_orders')
     conn = Tortoise.get_connection("default")
     result = await conn.execute_query("""
-        select l.id from marketplace_order l
-        join lateral (select * from marketplace_order where make_contract = l.make_contract and make_token_id = l.make_token_id and platform <> l.platform and last_updated_at > l.last_updated_at limit 1) o on true
-        where l.status = 'ACTIVE' and l.platform = 'RARIBLE_V1'
+        select l.id, l.maker, l.make_value, coalesce(o.balance, 0) balance from marketplace_order l
+                left join lateral (select * from ownership
+                                   where contract = l.make_contract and token_id = l.make_token_id and owner = l.maker limit 1
+            ) o on true
+        where l.status = 'ACTIVE' and l.platform = 'RARIBLE_V1' and coalesce(o.balance, 0) < (l.make_value - l.fill)
         limit $1
     """, [int(batch)])
+
+    # clear all with 0 balance
+    # result = await conn.execute_query("""
+    #     select l.id, l.maker, l.make_value, coalesce(o.balance, 0) balance from marketplace_order l
+    #             left join lateral (select * from ownership
+    #                                where contract = l.make_contract and token_id = l.make_token_id and owner = l.maker limit 1
+    #         ) o on true
+    #     where l.status = 'ACTIVE' and l.platform = 'RARIBLE_V1' and coalesce(o.balance, 0)  = 0
+    #     limit $1
+    # """, [int(batch)])
     if result[0] > 0:
         logger.info(f'Found {result[0]} outdated orders')
         for row in result[1]:
