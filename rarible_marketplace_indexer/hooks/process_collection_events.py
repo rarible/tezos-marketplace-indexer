4ef827f24d32dfd92a49cc2cf3bbbbb6022330e4
Roman Novikov <mystdeim@gmail.com>
2023-02-27 15:33:26 +0000

    fix minters

--- rarible_marketplace_indexer/hooks/process_collection_events.py
+++ rarible_marketplace_indexer/hooks/process_collection_events.py
@@ -12,66 +12,91 @@ from rarible_marketplace_indexer.types.tezos_objects.tezos_object_hash import Or
 from rarible_marketplace_indexer.utils.rarible_utils import date_pattern
 
 
+logger = logging.getLogger('dipdup.process_collection_events')
+
+
 async def process_collection_events(
     ctx: HookContext,
     level: int,
 ) -> None:
-    logger = logging.getLogger('dipdup.collection')
-    tzkt = ctx.get_tzkt_datasource('tzkt')
     index = await IndexingStatus.get_or_none(index=IndexEnum.COLLECTION)
     current_level = int(index.last_level) if index is not None else level
-
     logger.info(f"Processing collections from level {current_level}")
 
     last_id = 0
-    cr_filter = ""
     while last_id is not None:
-        originations = await tzkt.request(
-            method='get', url=f"v1/operations/originations?limit=100&level.gt={current_level}{cr_filter}"
-        )
-        for origination in originations:
-            if origination.get("originatedContract") is not None:
-                if origination["originatedContract"].get("address") is not None:
-                    contract = await tzkt.request(
-                        method='get', url=f"v1/contracts/{origination['originatedContract']['address']}"
-                    )
-                    current_level = origination['level']
-                    last_id = origination['id']
-                    cr_filter = f"&id.cr={last_id}"
-                    if "tzips" in contract and 'fa2' in contract['tzips']:
-                        if "alias" in contract:
-                            origination['alias'] = contract['alias']
-                        else:
-                            origination['alias'] = ""
-                        address = origination['originatedContract']['address']
-                        collection = await Collection.get_or_none(id=OriginatedAccountAddress(address))
-                        if collection is None:
-                            await Collection.create(
-                                id=OriginatedAccountAddress(address),
-                                owner=ImplicitAccountAddress(origination['sender']['address']),
-                                db_updated_at=datetime.now().strftime(date_pattern),
-                                name=origination['alias'],
-                                minters=[],
-                                standard='fa2',
-                                symbol=None,
-                            )
-                            collection_metadata = await CollectionMetadata.get_or_none(
-                                id=OriginatedAccountAddress(address)
-                            )
-                            if collection_metadata is None:
-                                await CollectionMetadata.create(
-                                    id=OriginatedAccountAddress(address),
-                                    metadata=None,
-                                    metadata_synced=False,
-                                    metadata_retries=0,
-                                    db_updated_at=datetime.now().strftime(date_pattern),
-                                )
-
-                            logger.info(f"Proccessed collection {address}")
+        current_level, last_id, total = await process_originations(ctx, current_level, last_id)
         if index is None:
-            await IndexingStatus.create(index=IndexEnum.COLLECTION, last_level=level)
+            await IndexingStatus.create(index=IndexEnum.COLLECTION, last_level=current_level)
         else:
-            index.last_level = level
+            index.last_level = current_level
             await index.save()
-        if len(originations) < 100:
+        if total < 1000:
             last_id = None
+
+async def process_originations(ctx, current_level, last_id):
+    tzkt = ctx.get_tzkt_datasource('tzkt')
+    cr_filter = f"&id.gt={last_id}"
+    logger.info(f"Get originations: from {current_level}{cr_filter}")
+    originations = await tzkt.request(
+        method='get', url=f"v1/operations/originations?limit=1000&sort.id=asc&level.ge={current_level}{cr_filter}"
+    )
+    for origination in originations:
+        if origination.get("originatedContract") is not None:
+            if origination["originatedContract"].get("address") is not None:
+                contract = await tzkt.request(
+                    method='get', url=f"v1/contracts/{origination['originatedContract']['address']}"
+                )
+                current_level = origination['level']
+                last_id = origination['id']
+                if "tzips" in contract and 'fa2' in contract['tzips']:
+                    if "alias" in contract:
+                        origination['alias'] = contract['alias']
+                    else:
+                        origination['alias'] = ""
+                    address = origination['originatedContract']['address']
+                    collection = await Collection.get_or_none(id=OriginatedAccountAddress(address))
+                    origin_minters = minters(origination)
+                    if collection is None:
+                        await Collection.create(
+                            id=OriginatedAccountAddress(address),
+                            owner=ImplicitAccountAddress(origination['sender']['address']),
+                            db_updated_at=datetime.now().strftime(date_pattern),
+                            name=name(origination),
+                            minters=origin_minters,
+                            standard='fa2',
+                            symbol=None,
+                        )
+                        collection_metadata = await CollectionMetadata.get_or_none(
+                            id=OriginatedAccountAddress(address)
+                        )
+                        if collection_metadata is None:
+                            await CollectionMetadata.create(
+                                id=OriginatedAccountAddress(address),
+                                metadata=None,
+                                metadata_synced=False,
+                                metadata_retries=0,
+                                db_updated_at=datetime.now().strftime(date_pattern),
+                            )
+                    else:
+                        if collection.minters != origin_minters:
+                            collection.minters = origin_minters
+                            collection.save()
+                            logger.info(f"Saved minters to {address}")
+    return current_level, last_id, len(originations)
+
+def minters(origination):
+    if 'initiator' in origination:
+        return [origination['initiator']['address']]
+    else:
+        return [origination['sender']['address']]
+
+def name(origination):
+    if 'alias' in origination:
+        return origination['alias']
+    elif 'alias' in origination['contractManager']:
+        return origination['contractManager']['alias']
+    elif 'alias' in origination['originatedContract']:
+        return origination['originatedContract']['alias']
+    else:
+        return ""
---
+++ rarible_marketplace_indexer/hooks/reindex_collections.py
@@ -0,0 +1,38 @@
+import logging
+import traceback
+
+from dipdup.context import HookContext
+
+from rarible_marketplace_indexer.enums import TaskStatus
+from rarible_marketplace_indexer.hooks.process_collection_events import process_originations
+from rarible_marketplace_indexer.models import Tasks
+
+logger = logging.getLogger("dipdup.reindex_collections")
+
+
+async def reindex_collections(ctx: HookContext, id: int) -> None:
+    task = await Tasks.get_or_none(id=id)
+    batch = int(ctx.config.hooks.get('reindex_collections').args.get("batch"))
+    try:
+        task.status = TaskStatus.RUNNING
+        await task.save()
+        current_level = int(task.sample or task.param)
+        last_id = 0
+        total = 0
+        for step in range(batch):
+            current_level, last_id, total = await process_originations(ctx, current_level, last_id)
+            if total < 1000:
+                break
+        if total > 0:
+            task.sample = current_level
+        else:
+            logger.info(f"Task={task.name} finished")
+            task.status = TaskStatus.FINISHED
+    except Exception as err:
+        str = traceback.format_exc()
+        task.error = str
+        task.status = TaskStatus.FAILED
+        logger.error(f"Task={task.name} failed with {err}")
+        logger.error(f"Task={task.name} trace: {str}")
+    task.version += 1
+    await task.save()
