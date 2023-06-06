cfdd8134a3f693e66ba76af50ed8676ed23195c6
Roman Novikov <mystdeim@gmail.com>
2023-06-06 19:41:41 +0100

    recreate indexes

--- rarible_marketplace_indexer/sql/migration/0_remove_too_big_token_ids.sql
+++ rarible_marketplace_indexer/sql/migration/0_remove_too_big_token_ids.sql
@@ -9,7 +9,12 @@
 -- delete from marketplace_activity where length(take_token_id) > 256;
 --
 --
---
---
--- remove frozen tasks
-update tasks set status = 'CANCELED' where status = 'RUNNING' and sample is null;
+drop index idx_marketplace_activity_by_hash;
+drop index idx_metadata_co_metadat_721bd1;
+drop index idx_metadata_to_metadat_caffa0;
+drop index metadata_retries_synced;
+drop index token_transfer_date;
+drop index uid_dipdup_toke_network_5d1a25;
+drop index uid_dipdup_cont_network_1ae32f;
+drop index idx_aggregator__tracker_66ec96;
+drop index idx_aggregator__level_96452e;
